52c55da0c9cb1e8cb10aa3417c5aa0d7cdf65efb
Mei Wong <fongmeiw@gmail.com>
2019-10-09 05:20:52 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ def line(katz_deli)
     message = "The line is currently:"
     line = ""
     while i < katz_deli.size do
-      line = " #{i+1}. #{katz_deli[i]}"
+      line = line + " #{i+1}. #{katz_deli[i]}"
       i += 1
     end
     
